f42aad6877d5fb9e2d1f38b88f8ce373062e8e26
leegm0430 <leegm0430@gmail.com>
2024-04-14 20:35:24 +0900

    HW06

--- HW/HW06/main.js
+++ HW/HW06/main.js
@@ -2,35 +2,68 @@
 var canvas = document.getElementById("GameScreenCanvas");
 var ctx = canvas.getContext("2d");
 
-// 하트를 그리는 함수
-function drawHeart(x, y, size, color) {
-  ctx.beginPath();
-  ctx.moveTo(x, y + size / 4);
-  ctx.quadraticCurveTo(x, y, x + size / 4, y);
-  ctx.quadraticCurveTo(x + size / 2, y, x + size / 2, y + size / 4);
-  ctx.quadraticCurveTo(x + size / 2, y, x + size * 3 / 4, y);
-  ctx.quadraticCurveTo(x + size, y, x + size, y + size / 4);
-  ctx.quadraticCurveTo(x + size, y + size / 2, x + size * 3 / 4, y + size * 3 / 4);
-  ctx.lineTo(x + size / 2, y + size);
-  ctx.lineTo(x + size / 4, y + size * 3 / 4);
-  ctx.quadraticCurveTo(x, y + size / 2, x, y + size / 4);
-  ctx.fillStyle = color;
-  ctx.fill();
+var colors = ["#C7C5FF", "black", "blue", "magenta", "pink", "cyan", "orange"];
+
+class Heartobject {
+    constructor(colorIndex, size, positionX, positionY) {
+        this.color = colors[colorIndex];
+        this.size = size;
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+    draw() {
+        ctx.save();
+        ctx.beginPath();
+
+        ctx.translate(this.positionX, this.positionY);
+        ctx.scale(this.size, this.size);
+
+        ctx.moveTo(0, 0.4);
+        ctx.lineTo(-0.3, 0.1);
+        ctx.arc(-0.1, 0.1, 0.2, Math.PI, 0, false);
+        ctx.arc(0.2, 0.1, 0.2, Math.PI, 0, false);
+        ctx.lineTo(0, 0.4);
+
+        ctx.fillStyle = this.color;
+        ctx.fill();
+
+        ctx.closePath();
+        ctx.restore();
+    }
 }
 
-// 무작위 위치와 크기를 갖는 하트를 그립니다.
-function drawRandomHeart() {
-  var x = Math.random() * canvas.width;
-  var y = Math.random() * canvas.height;
-  var size = Math.random() * 50 + 30; // 크기는 최소 30부터 80까지 무작위로 선택합니다.
-  var color = getRandomColor();
-  drawHeart(x, y, size, color);
+// 하트 객체를 담을 배열 생성
+var hearts = [];
+
+// 마우스 이벤트 리스너 추가
+canvas.addEventListener('mousemove', function(event) {
+    var mouseX = event.clientX - canvas.getBoundingClientRect().left;
+    var mouseY = event.clientY - canvas.getBoundingClientRect().top;
+
+    // 총 하트 개수가 100개가 넘지 않도록 조절
+    if (hearts.length < 100) {
+        // 배열에 하트 객체 추가
+        hearts.push(new Heartobject(Math.round(Math.random() * 6), Math.random() * 100, mouseX, mouseY));
+    } else {
+        // 배열의 맨 처음에 있는 하트 제거
+        hearts.shift();
+        // 새로운 하트 객체 추가
+        hearts.push(new Heartobject(Math.round(Math.random() * 6), Math.random() * 100, mouseX, mouseY));
+    }
+});
+
+// 렌더링 함수
+function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // 캔버스 지우기
+
+    // 배열의 각 하트 객체를 반복하여 그리기
+    hearts.forEach(function(heart) {
+        heart.draw();
+    });
+
+    requestAnimationFrame(render);
 }
 
-// 랜덤한 색상 반환
-function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-// 1초에 한 번씩 무작위 하트 그리기
-setInterval(drawRandomHeart, 1000);
+// 렌더링 시작
+render();
--- HW/HW06/main3.js
+++ HW/HW06/main3.js
@@ -1,69 +1,35 @@
-// Canvas Element 불러오기
+//Canvas Element 불러오기
 var canvas = document.getElementById("GameScreenCanvas");
 var ctx = canvas.getContext("2d");
 
-var colors = ["#C7C5FF", "black", "blue", "magenta", "pink", "cyan", "orange"];
-
-class Heartobject {
-    constructor(colorIndex, size, positionX, positionY) {
-        this.color = colors[colorIndex];
-        this.size = size;
+class HeartObject{
+    constructor(col, radius, positionX, positionY){
+        this.color = col;
+        this.radius = radius;
         this.positionX = positionX;
         this.positionY = positionY;
     }
-
-    draw() {
-        ctx.save();
+    
+    draw(){
         ctx.beginPath();
-
-        ctx.translate(this.positionX, this.positionY);
-        ctx.scale(this.size, this.size);
-
-        ctx.moveTo(0, 0.4);
-        ctx.lineTo(-0.3, 0.1);
-        ctx.arc(-0.1, 0.1, 0.2, Math.PI, 0, false);
-        ctx.arc(0.2, 0.1, 0.2, Math.PI, 0, false);
-        ctx.lineTo(0, 0.4);
-
+        for (var angle = 0; angle < Math.PI * 2; angle += 0.01) {
+        var x = this.positionX + this.radius * Math.cos(angle);
+        var y = this.positionY + this.radius * Math.sin(angle);
+        ctx.lineTo(x, y);
+        }
         ctx.fillStyle = this.color;
         ctx.fill();
-
-        ctx.closePath();
-        ctx.restore();
+        ctx.closePath();        
     }
+
 }
 
-// 하트 객체를 담을 배열 생성
-var hearts = [];
 
-// 마우스 이벤트 리스너 추가
-canvas.addEventListener('mousemove', function(event) {
-    var mouseX = event.clientX - canvas.getBoundingClientRect().left;
-    var mouseY = event.clientY - canvas.getBoundingClientRect().top;
 
-    // 총 하트 개수가 100개가 넘지 않도록 조절
-    if (hearts.length < 100) {
-        // 배열에 하트 객체 추가
-        hearts.push(new Heartobject(Math.round(Math.random() * 6), Math.random() * 100, mouseX, mouseY));
-    } else {
-        // 배열의 맨 처음에 있는 하트 제거
-        hearts.shift();
-        // 새로운 하트 객체 추가
-        hearts.push(new Heartobject(Math.round(Math.random() * 6), Math.random() * 100, mouseX, mouseY));
-    }
-});
-
-// 렌더링 함수
-function render() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // 캔버스 지우기
-
-    // 배열의 각 하트 객체를 반복하여 그리기
-    hearts.forEach(function(heart) {
-        heart.draw();
-    });
-
-    requestAnimationFrame(render);
+function draw(){    
+    var Circle = new HeartObject("blue", Math.random()*100, Math.random()*500, Math.random()*500);
+    Circle.draw();
+    requestAnimationFrame(draw);
 }
 
-// 렌더링 시작
-render();
+draw();
